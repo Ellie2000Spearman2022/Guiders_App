38a4f868632ee8a863d63eaaff5401183024a2e0
Adrianna Azara <alatus2@uic.edu>
2022-01-25 16:39:42 -0600

    Initial Commit

---
+++ Guiders_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Guiders_App
+//
+//  Created by Adrianna  on 1/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
